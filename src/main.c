35586703f00c32e79c320561bb5f162346c0c8d5
Noustaa <noustaa@gmail.com>
2021-10-07 17:15:48 +0200

    Fix to work with linux terminal

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "struct_def.h"
+#include "struct_def.c"
 
 
 int main(void){
   int fileSizeShip1,fileSizeShip2;
-  char *shipFile1 = GetShip("../assets/vaisseaux/enemi/BadShip.txt", &fileSizeShip1);
-  char *shipFile2 = GetShip("../assets/vaisseaux/enemi/Enemy_MotherShip.txt", &fileSizeShip2);
+  char *shipFile1 = GetShip("../assets/Vaisseaux/enemi/BadShip.txt", &fileSizeShip1);
+  char *shipFile2 = GetShip("../assets/Vaisseaux/enemi/Enemy_MotherShip.txt", &fileSizeShip2);
   
   diplayShip(fileSizeShip1, shipFile1);
   diplayShip(fileSizeShip2, shipFile2);
