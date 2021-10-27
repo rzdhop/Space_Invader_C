783ba406415dec11ef0736426ec8d6b8eaa348c7
Noustaa <noustaa@gmail.com>
2021-10-27 16:11:33 +0200

    Implementing code in thread

--- src/main.c
+++ src/main.c
@@ -5,136 +5,110 @@
 #include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/ioctl.h>
+#include <pthread.h>
 #include "struct_def.h"
 #include "key_pressed.h"
 
-int main(void){
-  system("clear");
-  //init Thread 
+typedef struct args_struct_thread args_thread;
+struct args_struct_thread
+{
+    int y;
+    int x;
+    char missile;
+    char *a;
+};
+
+void *printinga(void* _threadArgs)
+{   
+  args_thread *args = (args_thread*) _threadArgs;
   int fileSizeShip1,fileSizeShip2;
   int y=1; //Vertical axis 
-  int x=0; //Horizontal axis
-  int ymax=48;
-  int xmax=192;
+  int x=2; //Horizontal axis
   char *shipFile1 = GetShip("../assets/Vaisseaux/ennemis/TestShip.txt", &fileSizeShip1);
   char *shipFile2 = GetShip("../assets/Vaisseaux/ennemis/Enemy_MotherShip.txt", &fileSizeShip2);  
   printf("\033[%d;%dH", y, x);
   diplayShip(fileSizeShip1, shipFile1, y, x);
-  char a=0;
   char missile=73;
-  int j1=0, j2=0, j3=0;
-  int fire1=0, fire2=0, fire3=0;
-  int t1, u1, t2, u2, t3, u3;
-  printf("\e[?25l"); //hide the terminal cursor
-
-  while (1)
-  {
-    if (fire1==1)
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  int ymax=w.ws_row-4;
+  int xmax=w.ws_col-7;
+    while (1)
     {
-      fireMissile(missile, t1+j1, u1);
-      j1++;
-      if (j1>50)
+      printf("%c", *(args->a));
+      if (args->a != 0) //Ship movement
       {
-        printf("\033[%d;%dH   ", t1+j1, u1);  
-        fire1=0;
-        j1=0;
-      }    
-    }
-        if (fire2==1)
-    {
-      fireMissile(missile, t2+j2, u2);
-      j2++;
-      if (j2>50)
-      {
-        printf("\033[%d;%dH   ", t2+j2, u2);  
-        fire2=0;
-        j2=0;
-      }    
-    }  
-      if (fire3==1)
-    {
-      fireMissile(missile, t3+j3, u3);
-      j3++;
-      if (j3>50)
-      {
-        printf("\033[%d;%dH   ", t3+j3, u3);  
-        fire3=0;
-        j3=0;
-      }    
-    }
-    a=key_pressed();
-    if (a != 0) //Ship movement
-    {
-        if (a == 122)//Up - Z key
-      {
-        if ((y-4)>1)
+          if (*(args->a) == 122)//Up - Z key
         {
-        eraseShip(fileSizeShip1, shipFile1, y, x);
-        y=y-4;
-        printf("\033[%d;%dH", y, x);
-        diplayShip(fileSizeShip1, shipFile1, y, x);
-        printf("\033[%d;%dH", y, x);
+          if ((y-4)>0)
+          {
+          eraseShip(fileSizeShip1, shipFile1, y, x);
+          y=y-4;
+          printf("\033[%d;%dH", y, x);
+          diplayShip(fileSizeShip1, shipFile1, y, x);
+          printf("\033[%d;%dH", y, x);
+          }
         }
-      }
-      if (a == 115)//Down - S key
-      {
-        if ((y+4)<50)
+        if (*(args->a) == 115)//Down - S key
         {
-        eraseShip(fileSizeShip1, shipFile1, y, x);
-        y=y+4;
-        printf("\033[%d;%dH", y, x);
-        diplayShip(fileSizeShip1, shipFile1, y, x);
-        printf("\033[%d;%dH", y, x);
+          if ((y+4)<ymax)
+          {
+          eraseShip(fileSizeShip1, shipFile1, y, x);
+          y=y+4;
+          printf("\033[%d;%dH", y, x);
+          diplayShip(fileSizeShip1, shipFile1, y, x);
+          printf("\033[%d;%dH", y, x);
+          }
         }
-      }
-      if (a == 113)//Left - Q key
-      {
-        if ((x-4)>0)
+        if (*(args->a) == 113)//Left - Q key
         {
-        eraseShip(fileSizeShip1, shipFile1, y, x);
-        x=x-4;
-        printf("\033[%d;%dH", y, x);
-        diplayShip(fileSizeShip1, shipFile1, y, x);
-        printf("\033[%d;%dH", y, x);
+          if ((x-4)>0)
+          {
+          eraseShip(fileSizeShip1, shipFile1, y, x);
+          x=x-4;
+          printf("\033[%d;%dH", y, x);
+          diplayShip(fileSizeShip1, shipFile1, y, x);
+          printf("\033[%d;%dH", y, x);
+          }
         }
-      }
-      if (a == 100)//Right - D key
-      {
-        if ((x+4)<xmax)
+        if (*(args->a) == 100)//Right - D key
         {
-        eraseShip(fileSizeShip1, shipFile1, y, x);
-        x=x+4;
-        printf("\033[%d;%dH", y, x);
-        diplayShip(fileSizeShip1, shipFile1, y, x);
-        printf("\033[%d;%dH", y, x);        }
-      }
-      if (a == 32)//Fire missile - Space key
-      {
-        if (fire1==0)
-        {
-          t1=y+5;
-          u1=x+4;
-          fire1=1;
-          continue;
-        }  
-        if (fire2==0)
-        {
-          t2=y+5;
-          u2=x+4;
-          fire2=1; 
-          continue;
+          if ((x+4)<xmax)
+           {
+          eraseShip(fileSizeShip1, shipFile1, y, x);
+          x=x+4;
+          printf("\033[%d;%dH", y, x);
+          diplayShip(fileSizeShip1, shipFile1, y, x);
+          printf("\033[%d;%dH", y, x);        }
         }
-        if (fire3==0)
+        if (*(args->a) == 32)//Fire - Space key
         {
-          t3=y+5;
-          u3=x+4;
-          fire3=1; 
-          continue;
+          for (int i = 0; i < 50; i++)
+          {
+              printf("\033[%d;%dH    ", y+i, x);  
+              printf("\033[%d;%dH%c", y+i+1, x, missile);  
+              fflush(stdout);
+              usleep(7000);
+          }
         }
-      }
     }
   }
-  //diplayShip(fileSizeShip2, shipFile2);
+  return NULL;
+}
 
+int main(void){
+  char *a;
+  system("clear");
+  printf("\e[?25l"); //hide the terminal cursor
+  pthread_t thread_id[2];
+  args_thread *args = (args_thread*)malloc(sizeof(args_thread));
+  args->a = a;
+  pthread_create(&thread_id[0], NULL, printinga, (void *)args);
+  while (1)
+  {
+    *a=key_pressed();
+  }
+  
   return 0;
 }
